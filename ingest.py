116be6a202bc4e6533f7926b581b8ba64b56a571
SrinidhiAvantikaaR <e3srini.ar@gmail.com>
2026-07-02 17:30:27 +0530

    Modified ingest.py file

--- ingest.py
+++ ingest.py
@@ -1,6 +1,8 @@
 import pymupdf
 import numpy as np
 import faiss
+import os
+import json
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from sentence_transformers import SentenceTransformer
 
@@ -10,6 +12,24 @@ splitter = RecursiveCharacterTextSplitter(
     chunk_overlap=100,
     separators=["\n\n", "\n", ".", " ", ""]
 )
+
+
+def save_faiss(index, chunks, path="vector_store"):
+    os.makedirs(path, exist_ok=True)
+
+    faiss.write_index(index, f"{path}/index.faiss")
+
+    with open(f"{path}/chunks.json", "w", encoding="utf-8") as f:
+        json.dump(chunks, f, ensure_ascii=False, indent=2)
+
+def load_faiss(path="vector_store"):
+    index = faiss.read_index(f"{path}/index.faiss")
+
+    with open(f"{path}/chunks.json", "r", encoding="utf-8") as f:
+        chunks = json.load(f)
+
+    return index, chunks
+
 
 def file_embedding(file_list: list):
     chunks = []
@@ -37,7 +57,9 @@ def file_embedding(file_list: list):
     faiss.normalize_L2(embeddings)
     index = faiss.IndexFlatIP(embeddings.shape[1])
     index.add(embeddings)
-   
+
+    save_faiss(index, chunks, path = "vector_score")
+    
     return index, chunks
 
 file_list = ['.pdf', 'sample_test_document.pdf']
